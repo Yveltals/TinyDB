866cabfff35ca2c2af030d446f8457c205ef11db
Yveltals <1302540061@qq.com>
2025-02-20 17:10:25 +0000

    add bloom filter

---
+++ include/tinydb/filter_policy.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+namespace tinydb {
+
+class Slice;
+
+class FilterPolicy {
+ public:
+  virtual ~FilterPolicy();
+
+  virtual const char* Name() const = 0;
+  virtual void CreateFilter(const Slice* keys, int n,
+                            std::string* dst) const = 0;
+  virtual bool KeyMayMatch(const Slice& key, const Slice& filter) const = 0;
+};
+
+// Callers must delete the result after any database that is using the
+// result has been closed.
+const FilterPolicy* NewBloomFilterPolicy(int bits_per_key);
+
+}  // namespace tinydb
